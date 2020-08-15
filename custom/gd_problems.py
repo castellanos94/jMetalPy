acae4e4c80ce0e4aa39c1de9ff571bf8c668e07b
aca810@gmail.com <aca810@gmail.com>
2020-08-15 13:55:48 -0500

    Refactored Problems that uses a Instance file to class GDProblem

---
+++ custom/gd_problems.py
@@ -0,0 +1,120 @@
+import random
+from abc import ABC
+from math import pi, cos
+from typing import TypeVar
+
+from custom.instance import DTLZInstance, Instance, PspInstance
+from jmetal.core.problem import Problem
+from jmetal.core.solution import FloatSolution, BinarySolution
+
+S = TypeVar('S')
+
+
+class GDProblem(Problem[S], ABC):
+    def __init__(self, instance_: Instance):
+        super(GDProblem, self).__init__()
+        self.instance_ = instance_
+        self.number_of_variables = instance_.n_var
+        self.number_of_objectives = instance_.n_obj
+        self.number_of_constraints = instance_.n_constraints
+
+
+class BinaryProblemGD(GDProblem[BinarySolution], ABC):
+    """ Class representing binary problems. """
+
+    def __init__(self, instance_):
+        super(BinaryProblemGD, self).__init__(instance_)
+        self.number_of_bits = instance_.n_var
+        self.number_of_variables = 1
+
+    def create_solution(self) -> BinarySolution:
+        new_solution = BinarySolution(number_of_variables=self.number_of_variables,
+                                      number_of_objectives=self.number_of_objectives)
+
+        new_solution.variables[0] = \
+            [True if random.randint(0, 1) == 0 else False for _ in range(
+                self.number_of_bits)]
+
+        return new_solution
+
+
+class FloatProblemGD(GDProblem[FloatSolution], ABC):
+    """ Class representing float problems. """
+
+    def __init__(self, instance_):
+        super(FloatProblemGD, self).__init__(instance_)
+        self.lower_bound = []
+        self.upper_bound = []
+
+    def create_solution(self) -> FloatSolution:
+        new_solution = FloatSolution(
+            self.lower_bound,
+            self.upper_bound,
+            self.number_of_objectives,
+            self.number_of_constraints)
+        new_solution.variables = \
+            [random.uniform(self.lower_bound[index_var] * 1.0, self.upper_bound[index_var] * 1.0) for index_var in
+             range(self.number_of_variables)]
+
+        return new_solution
+
+
+class DTLZ1Preferences(FloatProblemGD):
+    """ Problem DTLZ1Preferences. Continuous problem having a flat Pareto front
+
+        """
+
+    def __init__(self, instance_: DTLZInstance):
+        """ :param instance_: define number_of_variables and objectives also initial solution
+        """
+        super(DTLZ1Preferences, self).__init__(instance_)
+        self.obj_directions = [self.MINIMIZE] * self.number_of_objectives
+        self.obj_labels = ['$ f_{} $'.format(index_var) for index_var in range(self.number_of_objectives)]
+
+        self.lower_bound = self.number_of_variables * [0.0]
+        self.upper_bound = self.number_of_variables * [1.0]
+
+    def evaluate(self, solution: FloatSolution) -> FloatSolution:
+        k = self.number_of_variables - self.number_of_objectives + 1
+
+        g = sum([(x - 0.5) * (x - 0.5) - cos(20.0 * pi * (x - 0.5))
+                 for x in solution.variables[self.number_of_variables - k:]])
+
+        g = 100 * (k + g)
+
+        solution.objectives = [(1.0 + g) * 0.5] * self.number_of_objectives
+
+        for index_var in range(self.number_of_objectives):
+            for j in range(self.number_of_objectives - (index_var + 1)):
+                solution.objectives[index_var] *= solution.variables[j]
+
+            if index_var != 0:
+                solution.objectives[index_var] *= 1 - solution.variables[self.number_of_objectives - (index_var + 1)]
+
+        return solution
+
+    def get_name(self):
+        return 'DTLZ1Preferences'
+
+
+class PortfolioSocialProblem(BinaryProblemGD):
+    def __init__(self, instance_: PspInstance):
+        super(PortfolioSocialProblem, self).__init__(instance_)
+        self.budget = instance_.budget
+
+    def evaluate(self, solution: BinarySolution) -> BinarySolution:
+        budget = 0
+        objectives = self.number_of_objectives*[0.0]
+
+        for index, bits in enumerate(solution.variables[0]):
+            if bits:
+                budget += self.instance_.projects[index][0]
+                for obj in range(0, self.number_of_objectives):
+                    objectives[obj] += self.instance_.projects[index][obj + 3]
+        solution.objectives = [-1 * obj for obj in objectives]
+
+        solution.constraints = [self.budget - budget]
+        return solution
+
+    def get_name(self) -> str:
+        return 'PortfolioSocialProblem'
--- custom/test.py
+++ custom/test.py
@@ -1,19 +1,30 @@
-from custom.psp import MyProblem
-from custom.utils import print_solutions_to_file, DIRECTORY_RESULTS
+import random
+
+from custom.gd_problems import PortfolioSocialProblem, DTLZ1Preferences
+from custom.instance import PspInstance, DTLZInstance
+from custom.utils import print_solutions_to_file, DIRECTORY_RESULTS, ReferenceDirectionFromSolution
 from jmetal.algorithm.multiobjective import NSGAII
-from jmetal.operator import BitFlipMutation, SPXCrossover
+from jmetal.algorithm.multiobjective.nsgaiii import NSGAIII
+from jmetal.lab.visualization import Plot
+from jmetal.operator import BitFlipMutation, SPXCrossover, PolynomialMutation, SBXCrossover
 from jmetal.util.comparator import DominanceComparator
 from jmetal.util.observer import ProgressBarObserver
+from jmetal.util.ranking import FastNonDominatedRanking
 from jmetal.util.termination_criterion import StoppingByEvaluations
 
-if __name__ == '__main__':
-    problem = MyProblem()
-    max_evaluations = 1000
-    binary_string_length = problem.number_of_variables
+
+def psp_test():
+    path = '/home/thinkpad/Documents/jemoa/src/main/resources/instances/psp/o4p25_0.txt'
+    psp_instance = PspInstance()
+    psp_instance.read_(path)
+    max_evaluations = 50000
+    binary_string_length = psp_instance.n_var
+    problem = PortfolioSocialProblem(psp_instance)
+
     algorithm = NSGAII(
         problem=problem,
         population_size=100,
-        offspring_population_size=1,
+        offspring_population_size=100,
         mutation=BitFlipMutation(probability=1.0 / binary_string_length),
         crossover=SPXCrossover(probability=1.0),
         termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations),
@@ -21,6 +32,7 @@ if __name__ == '__main__':
     )
     progress_bar = ProgressBarObserver(max=max_evaluations)
     algorithm.observable.register(progress_bar)
+
     algorithm.run()
     front = algorithm.get_result()
     for s in front:
@@ -28,6 +40,63 @@ if __name__ == '__main__':
     # Save results to file
     # print_function_values_to_file(front, 'FUN.' + algorithm.get_name() + "-" + problem.get_name())
     print_solutions_to_file(front, DIRECTORY_RESULTS + 'SOLUTIONS.' + algorithm.get_name() + "-" + problem.get_name())
+
     print('Algorithm (continuous problem): ' + algorithm.get_name())
     print('Problem: ' + problem.get_name())
     print('Computing time: ' + str(algorithm.total_computing_time))
+
+
+def dtlz1_test():
+    instance = DTLZInstance()
+    path = '/home/thinkpad/Documents/jemoa/src/main/resources/instances/dtlz/DTLZInstance.txt'
+    instance.read_(path)
+    problem = DTLZ1Preferences(instance)
+
+    # problem.reference_front = read_solutions(filename='resources/reference_front/DTLZ2.3D.pf')
+
+    max_evaluations = 25000
+    experiment = 30
+    algorithm = NSGAIII(
+        problem=problem,
+        population_size=100,
+        reference_directions=ReferenceDirectionFromSolution(problem, problem.instance_),
+        mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
+        crossover=SBXCrossover(probability=1.0, distribution_index=30),
+        termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+    )
+    bag = []
+    for i in range(experiment):
+        algorithm = NSGAIII(
+            problem=problem,
+            population_size=92,
+            reference_directions=ReferenceDirectionFromSolution(problem, problem.instance_),
+            mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
+            crossover=SBXCrossover(probability=1.0, distribution_index=30),
+            termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+        )
+        progress_bar = ProgressBarObserver(max=max_evaluations)
+        algorithm.observable.register(progress_bar)
+        algorithm.run()
+
+        bag = bag + algorithm.get_result()
+    print(len(bag))
+    print_solutions_to_file(bag, DIRECTORY_RESULTS + 'Solutions.bag.' + algorithm.label)
+    ranking = FastNonDominatedRanking()
+
+    ranking.compute_ranking(bag)
+    front = ranking.get_subfront(0)
+    print(len(front))
+    # Save results to file
+    print_solutions_to_file(front, DIRECTORY_RESULTS + 'front0.' + algorithm.label)
+
+    print(f'Algorithm: ${algorithm.get_name()}')
+    print(f'Problem: ${problem.get_name()}')
+    print(f'Computing time: ${algorithm.total_computing_time}')
+    plot_front = Plot(title='Pareto front approximation', axis_labels=['x', 'y', 'z'])
+    plot_front.plot(front, label='NSGAII-ZDT1-preferences_bag', filename=DIRECTORY_RESULTS + 'NSGAII-ZDT1-p_f0',
+                    format='png')
+
+
+if __name__ == '__main__':
+    random.seed(8435)
+    psp_test()
